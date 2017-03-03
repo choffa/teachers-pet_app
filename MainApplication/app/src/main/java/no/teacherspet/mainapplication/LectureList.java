39000bd1fc998aaf86f4d2dbff796631b6a5f5b0
Magnleik <magnleik@stud.ntnu.no>
2017-03-03 12:22:03 +0100

    Added class for lecture fixed bug with back button started creation of lectures interface

--- MainApplication/app/src/main/java/no/teacherspet/mainapplication/LectureList.java
+++ MainApplication/app/src/main/java/no/teacherspet/mainapplication/LectureList.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 
 import java.util.ArrayList;
 
+import backend.Lecture;
+
 /**
  * Created by magnus on 22.02.2017.
  */
@@ -17,6 +19,12 @@ public class LectureList extends ListActivity {
     ArrayList<String> listItems=new ArrayList<>();
     ArrayAdapter<String> adapter;
     private EditText medit;
+    private boolean isStart;
+    Lecture L;
+    private int start;
+    private int end;
+    private String name;
+    private String room;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -24,6 +32,10 @@ public class LectureList extends ListActivity {
         adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,listItems);
         setListAdapter(adapter);
         medit= (EditText) findViewById(R.id.lecture_name);
+    }
+
+    void setName(String name){
+        Lecture.setName(name);
     }
 
     public void addItems(View v){
