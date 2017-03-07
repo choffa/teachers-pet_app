1fb1d16eacc05899d09cdbe5362c1d8e9762b97a
Magnleik <magnleik@stud.ntnu.no>
2017-03-07 13:53:14 +0100

    added subject name to header of ratingviews

--- MainApplication/app/src/main/java/no/teacherspet/mainapplication/StudentRating.java
+++ MainApplication/app/src/main/java/no/teacherspet/mainapplication/StudentRating.java
@@ -20,7 +20,7 @@ public class StudentRating extends AppCompatActivity {
     RadioGroup tempo;
     HashMap<String,ArrayList<Integer>> savedLectures=new HashMap<>();
     TextView hello; //Textfield only for debugging purposes: shows the last two values
-    StudentInfo stud=RoleSelect.getStud();
+    StudentInfo stud=RoleSelect.getStud(); //no longer necessary
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +30,7 @@ public class StudentRating extends AppCompatActivity {
         setContentView(R.layout.activity_student_rating);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(StudentLectureList.getName());
         tempo = (RadioGroup) findViewById(R.id.tempoRadioGroup);
         if(RoleSelect.saves.containsKey(StudentLectureList.getID())){
             tempo.check(RoleSelect.saves.get(lectureID).get(0));
