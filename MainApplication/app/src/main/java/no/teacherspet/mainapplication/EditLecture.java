43a44658bfe7a8754a3a033ec2cf1712616a7061
Eirik Osnes <eirik.osnes@gmail.com>
2017-03-21 12:49:10 +0100

    Created classes and views for edit and statistics

---
+++ MainApplication/app/src/main/java/no/teacherspet/mainapplication/EditLecture.java
@@ -0,0 +1,19 @@
+package no.teacherspet.mainapplication;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by eirik on 21.03.2017.
+ */
+
+public class EditLecture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lecture_edit);
+    }
+
+}
---
+++ MainApplication/app/src/main/java/no/teacherspet/mainapplication/LectureStatistics.java
@@ -0,0 +1,18 @@
+package no.teacherspet.mainapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by eirik on 21.03.2017.
+ */
+
+public class LectureStatistics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lecture_statistics);
+    }
+
+}
