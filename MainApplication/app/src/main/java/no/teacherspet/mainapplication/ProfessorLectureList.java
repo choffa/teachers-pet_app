e68ed2aeff8ac433569ef1dcd9274e0fd228166d
Eirik Osnes <eirik.osnes@gmail.com>
2017-04-15 20:38:16 +0200

    Major cleanup at aisle 9 (on ProfessorLectureList)

--- MainApplication/app/src/main/java/no/teacherspet/mainapplication/ProfessorLectureList.java
+++ MainApplication/app/src/main/java/no/teacherspet/mainapplication/ProfessorLectureList.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -24,67 +27,71 @@ import frontend.Connection;
 
 public class ProfessorLectureList extends AppCompatActivity {
 
- //   static ArrayList<String> listItems=new ArrayList<>();
     static ArrayList<Lecture> lecturesArray=new ArrayList<>();
     public static ArrayAdapter<Lecture> adapter;
-    private int start;
-    private int end;
     private static String Name;
-    private String room;
-    private Date date;
     private static int ID;
     Thread thread;
     protected Connection c;
     ListView list_view;
 
     protected void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.lectures);
-            thread=new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        c = new Connection();
-                    } catch (IOException e) {
-                        Toast.makeText(ProfessorLectureList.this, "Noe gikk galt under lasting av siden", Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lectures);
+        thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    c = new Connection();
+                } catch (IOException e) {
+                    Toast.makeText(ProfessorLectureList.this, "Noe gikk galt under lasting av siden", Toast.LENGTH_SHORT).show();
+                    finish();
                 }
-            });
-            thread.start();
+            }
+        });
+        thread.start();
         try {
             thread.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         ActionBar actionBar = getSupportActionBar();
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            lecturesArray=c.getLectures(RoleSelect.ProfessorID);
-            ListView list_view = (ListView) findViewById(android.R.id.list);
-            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lecturesArray);
-            list_view.setAdapter(adapter);
-            list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-           @Override
-           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-               Intent myIntent=new Intent(ProfessorLectureList.this,ProfessorLive.class);
-               Lecture L= (Lecture) list_view.getItemAtPosition(position);
-               ID=L.getID();
-               Name = L.getCourseID();
-               switch (beforeNowAfter(L.getDate(),L.getStart(),L.getEnd())) {
-                   case 0:
-                       myIntent = new Intent(ProfessorLectureList.this, LectureStatistics.class);
-                       break;
-                   case 1:
-                       myIntent = new Intent(ProfessorLectureList.this, ProfessorLive.class);
-                       break;
-                   case 2:
-                       myIntent = new Intent(ProfessorLectureList.this, EditLecture.class);
-                       break;
-               }
-               ProfessorLive.setID(ID);
-               startActivity(myIntent);
-           }
-       });
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        lecturesArray=c.getLectures(RoleSelect.ProfessorID);
+        list_view = (ListView) findViewById(android.R.id.list);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lecturesArray);
+        initiateListAdapter(list_view,adapter);
+    }
+
+    /**
+     * Sets the onClick logic for listView's adapter listAdapter
+     * @param listView The ListView to set the logic for.
+     * @param listAdapter The ListAdapter to override onItemClick for.
+     */
+    private void initiateListAdapter(ListView listView, ListAdapter listAdapter){
+        listView.setAdapter(listAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent myIntent=new Intent(ProfessorLectureList.this,ProfessorLive.class);
+                Lecture L= (Lecture) listView.getItemAtPosition(position);
+                ID=L.getID();
+                Name = L.getCourseID();
+                switch (beforeNowAfter(L.getDate(),L.getStart(),L.getEnd())) {
+                    case 0:
+                        myIntent = new Intent(ProfessorLectureList.this, LectureStatistics.class);
+                        break;
+                    case 1:
+                        myIntent = new Intent(ProfessorLectureList.this, ProfessorLive.class);
+                        break;
+                    case 2:
+                        myIntent = new Intent(ProfessorLectureList.this, EditLecture.class);
+                        break;
+                }
+                ProfessorLive.setID(ID);
+                startActivity(myIntent);
+            }
+        });
     }
 
     public void createBtnClicked(View v){
@@ -93,11 +100,11 @@ public class ProfessorLectureList extends AppCompatActivity {
     }
 
     /**
-     * returns 0 if lecture is done, 1 if ongoing, 2 if not yet started
-     * @param lectureDate
-     * @param start
-     * @param end
-     * @return
+     * Checks whether the lecture is currently active, is yet to start or is done.
+     * @param lectureDate Date of the Lecture
+     * @param start Start hour for the lecture
+     * @param end End hour for the lecture
+     * @return returns 0 if lecture is done, 1 if ongoing, 2 if not yet started
      */
     private int beforeNowAfter(Date lectureDate, int start, int end){
         Date now = new Date();
@@ -116,7 +123,7 @@ public class ProfessorLectureList extends AppCompatActivity {
                 }else if(lectureDate.getDate()>now.getDate()){
                     return 2;
                 }else{
-                    if(end==now.getHours()&&now.getMinutes()>15){
+                    if(end==now.getHours()&&now.getMinutes()>15){ //Lectures end officially after 15min past the end hour.
                         return 0;
                     }else if(end<now.getHours()){
                         return 0;
@@ -130,18 +137,15 @@ public class ProfessorLectureList extends AppCompatActivity {
         }
     }
 
-
-
     /**
-     * adds an item to the listview
-     * @param v: the view that the item will be added to.
+     * Updates the list to check for new lectures.
      */
-
-    public void addItems(View v){
-      //  listItems.add(Name);
-        lecturesArray.add(new Lecture(Name,start,end,room,date));
+    private void update(){
+        lecturesArray.clear();
+        lecturesArray.addAll(c.getLectures(RoleSelect.ProfessorID));
         adapter.notifyDataSetChanged();
     }
+
     public static int getID(){
         return ProfessorLectureList.ID;
     }
@@ -155,16 +159,10 @@ public class ProfessorLectureList extends AppCompatActivity {
         return true;
     }
 
-    private void update(){
-        lecturesArray.clear();
-        lecturesArray.addAll(c.getLectures(RoleSelect.ProfessorID));
-        adapter.notifyDataSetChanged();
-    }
     @Override
     public void onResume(){
         super.onResume();
         update();
-
     }
 
     @Override
